1e48cd04cf7f93f17fff7b17b63f28f2447084dc
steevmi1 <steeves@raingods.net>
2016-09-01 14:11:37 -0400

    First two HW problems done.

--- MachineLearning/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ MachineLearning/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,7 +19,11 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
+thetasq = theta(2:end) .^ 2;
+temp = theta;
+temp(1) = 0;
+J = 1 / (2 * m) * sum((X * theta - y).^2) + (lambda/(2*m)) * sum(thetasq(:));
+grad = (1 / m) * (X' * (X * theta - y)) .+ (lambda/m) .* temp;
 
 
 
